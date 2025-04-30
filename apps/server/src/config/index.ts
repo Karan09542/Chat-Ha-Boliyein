c4b950ba7170512dc7894c274d6916689b2dd7d0
Karan09542 <Karan09542@gamil.com>
2025-04-30 08:44:57 +0530

    सम्भू

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -8,6 +8,6 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 export const PORT = process.env.PORT || 1008;
 export const IS_PROD = process.env.NODE_ENV === "production";
 
-export const REDIS_URL = IS_PROD ? `${process.env.REDIS_URL}` : `redis://${getIpv4Address()}:6379`;
+export const REDIS_URL = IS_PROD ? `${process.env.REDIS_URL?.replace('<redis_password>', process.env.REDIS_PASSWORD as string)}` : `redis://${getIpv4Address()}:6379`;
 
 export const DB_URL = IS_PROD ? `${process.env.DB_URL?.replace('<db_password>', process.env.DB_PASSWORD as string)}` : `mongodb://${getIpv4Address()}:27017`;
